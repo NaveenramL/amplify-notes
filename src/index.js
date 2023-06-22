6542c01baf304e0b2fc33d328349daecfe3168b2
Naveenram <naveenramlrn@gmail.com>
2023-06-22 05:33:22 +0530

    first publish
    first publish

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+import { AmplifyProvider } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { studioTheme } from './ui-components';
+
+Amplify.configure(config)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AmplifyProvider theme={studioTheme}>
     <App />
+    </AmplifyProvider>
   </React.StrictMode>
 );
 
